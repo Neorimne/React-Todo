ee57ad64b54fda291abe8771ff0005b8674a3a0a
Denis <neorimne@gmail.com>
2020-05-08 10:42:50 +0200

    Updates

---
+++ precache-manifest.0d383c0c184be4f2558509c19cae0c4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6725a75f0ac765aa1c4c6cab6b1c621",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "91fe3720b06f4857f7a5",
+    "url": "/React-Todo/static/css/main.6d894665.chunk.css"
+  },
+  {
+    "revision": "54327c7400944cdad010",
+    "url": "/React-Todo/static/js/2.3e865340.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Todo/static/js/2.3e865340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91fe3720b06f4857f7a5",
+    "url": "/React-Todo/static/js/main.9323ea94.chunk.js"
+  },
+  {
+    "revision": "da4504c09118e0b50e86",
+    "url": "/React-Todo/static/js/runtime-main.ab9b9f29.js"
+  },
+  {
+    "revision": "5d26906e8d998951bae37613c761d364",
+    "url": "/React-Todo/static/media/preloader.5d26906e.svg"
+  }
+]);
